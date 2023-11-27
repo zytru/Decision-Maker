e21d4b9b94c6fdc31043c4cd2442db4b3aaf93fe
snow <tj.fernanders04@gmail.com>
2023-11-27 18:32:49 -0500

    Wrote general code for how the program should run

---
+++ flip_coin.py
@@ -0,0 +1,20 @@
+import random
+
+def flip_coin():
+    user = input("Would you like to choose heads or tails?\n").lower()
+    coin = random.randint(0,1)
+    if user == "heads":
+        choice = 0
+        if choice == coin:
+            print("Lucky you, it's heads!")
+        else:
+            print("Unlucky it's tails!")
+    elif user == "tails":
+        choice = 1
+        if choice == coin:
+            print("Lucky you, it's tails!")
+        else:
+            print("Unlucky it's heads!")
+    else:
+        print("Please put in heads or tails")
+    
